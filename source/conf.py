e7b7d58d99ae36a6dd9278e7ee1a28753937b8f6
Red Boumghar <red@parametry.ai>
2023-08-25 14:28:17 +0000

    Initial bit with conf and first files.

---
+++ source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DeSpace QF'
+copyright = '2023 Creative Commons Attribution-ShareAlike 4.0 International License'
+author = 'Red Boumghar, Manu Olariu, Rod Mamin, Paul Burg, Adam Boyle'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_theme_options = {
+    # 'analytics_id': 'XXXXXXXXXX',
+    # 'analytics_anonymize_ip': True,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
---
+++ source/index.rst
@@ -0,0 +1,40 @@
+.. DeSpace QF documentation master file, created by
+   sphinx-quickstart on Fri Aug 25 13:13:00 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DeSpace QF's documentation!
+======================================
+
+
+DeSpace QF stands for Decentralized Space Quadratic Funding.
+
+The DeSpace QF initiative's purpose is to support decentralized projects related
+to space, to push for the betterment of technology and commercialization.
+It applies solidarity principles and is greatly inspired by regenerative finance
+principles.
+
+The community is composed of project leaders, project supporters and
+contributors, investors, space fans and other external supports.
+
+.. image:: picture.jpg
+   :width: 200px
+   :height: 100px
+   :scale: 50 %
+   :alt: alternate text
+   :align: right
+
+You, me, them, everybody.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
